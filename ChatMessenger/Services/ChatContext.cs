c2b8a04444107c2afce51990af5daebdf026f357
AzureAD\IvanNorin <ivan.norin@gmail.com>
2024-11-12 14:23:44 +0300

    first commit

---
+++ ChatMessenger/Services/ChatContext.cs
@@ -0,0 +1,12 @@
+﻿using ChatMessenger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatMessenger.Services;
+
+public class ChatContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    public ChatContext(DbContextOptions<ChatContext> options) : base(options) { }
+}
---
+++ ChatMessenger/Services/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatMessenger.Services;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
